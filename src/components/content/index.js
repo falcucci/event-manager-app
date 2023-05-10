21a532145c69192c39df35fb767ff9b8f30166c7
Alexsander Falcucci <alexsander.santos@luizalabs.com>
2023-05-10 14:33:15 +0200

    adds modal, row, checkbox, mail and password icons to the content component, allowing the user to open a modal to sign in with email and password.
    includes adding the modal, row, checkbox, mail and password components to the content component, and setting the visible state of the modal to true when the open modal button is pressed. additionally, the close handler is set to close the modal and log to the console when clicked. the mail and password icon components are also added for the input components.

--- src/components/content/index.js
+++ src/components/content/index.js
@@ -2,19 +2,81 @@ import {
   Text,
   Input,
   Button,
+  Modal,
   Loading,
+  Row,
+  Checkbox,
   Spacer,
   Grid,
 } from "@nextui-org/react";
+import { Mail, Password } from "../../components/icons";
 import { useState } from "react";
 
 export const Content = () => {
+  const [visible, setVisible] = useState(false);
+  const handler = () => setVisible(true);
+  const closeHandler = () => {
+    setVisible(false);
+    console.log("closed");
+  };
 
   return (
     <Grid.Container
       justify="center"
       css={{ alignContent: "center", alignItems: "center" }}>
       <Grid css={{ width: "60%" }}>
+        <Button auto color="warning" shadow onPress={handler}>
+          Open modal
+        </Button>
+        <Modal
+          closeButton
+          blur
+          aria-labelledby="modal-title"
+          open={visible}
+          onClose={closeHandler}>
+          <Modal.Header>
+            <Text id="modal-title" size={18}>
+              Event Manager {" "}
+              <Text b size={18}>
+                App
+              </Text>
+            </Text>
+          </Modal.Header>
+          <Modal.Body>
+            <Input
+              clearable
+              bordered
+              fullWidth
+              color="primary"
+              size="lg"
+              placeholder="Email"
+              contentLeft={<Mail fill="currentColor" />}
+            />
+            <Input
+              clearable
+              bordered
+              fullWidth
+              color="primary"
+              size="lg"
+              placeholder="Password"
+              contentLeft={<Password fill="currentColor" />}
+            />
+            <Row justify="space-between">
+              <Checkbox>
+                <Text size={14}>Remember me</Text>
+              </Checkbox>
+              <Text size={14}>Forgot password?</Text>
+            </Row>
+          </Modal.Body>
+          <Modal.Footer>
+            <Button auto flat color="error" onPress={closeHandler}>
+              Close
+            </Button>
+            <Button auto onPress={closeHandler}>
+              Sign in
+            </Button>
+          </Modal.Footer>
+        </Modal>
       </Grid>
     </Grid.Container>
   );
