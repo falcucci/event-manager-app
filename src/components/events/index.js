64143a4e7cf3c20bb6614d6512f1b2209375c7ea
Alexsander Falcucci <alexsander.santos@luizalabs.com>
2023-05-10 23:29:08 +0200

    add a real case to see the mockup

--- src/components/events/index.js
+++ src/components/events/index.js
@@ -1,10 +1,12 @@
 import { StyledBadge } from "../badge";
+import _ from "lodash";
+import dayjs from "dayjs";
 import {
   Table,
   Row,
   Col,
   Tooltip,
-  User,
+  event,
   Text,
 } from "@nextui-org/react";
 import {
@@ -14,178 +16,285 @@ import {
   DeleteIcon,
 } from "../icons";
 
+_.in
 export const Events = () => {
   const columns = [
-    { name: "NAME", uid: "name" },
-    { name: "ROLE", uid: "role" },
-    { name: "STATUS", uid: "status" },
+    { name : "ID", uid: "id" },
+    { name : "NAME", uid: "name" },
+    { name : "DESCRIPTION", uid: "description" },
+    { name : "STARTS", uid: "start_date" },
+    { name : "ENDS", uid: "end_date" },
+    { name : "STATUS", uid: "status" },
+    { name : "LOCATION", uid: "location" },
+    { name : "CREATED BY", uid: "created_by" },
     { name: "ACTIONS", uid: "actions" },
   ];
 
-  const users = [
+  const events = [
     {
       id: 1,
-      name: "Tony Reichert",
-      role: "CEO",
-      team: "Management",
+      name: "Event 1",
+      description: "This is the first event",
       status: "active",
-      age: "29",
-      avatar: "https://i.pravatar.cc/150?u=a042581f4e29026024d",
-      email: "tony.reichert@example.com",
+      start_date: "2024-01-01T00:00:00Z",
+      end_date: "2024-01-02T00:00:00Z",
+      location: "New York",
+      is_public: true,
+      created_at: "2024-01-01T00:00:00Z",
+      updated_at: "2024-01-01T00:00:00Z",
+      created_by: 1,
+      subscribers: [1, 2, 3],
     },
     {
       id: 2,
-      name: "Zoey Lang",
-      role: "Technical Lead",
-      team: "Development",
-      status: "paused",
-      age: "25",
-      avatar: "https://i.pravatar.cc/150?u=a042581f4e29026704d",
-      email: "zoey.lang@example.com",
+      name: "Event 2",
+      description: "This is the second event",
+      status: "active",
+      start_date: "2024-02-01T00:00:00Z",
+      end_date: "2024-02-02T00:00:00Z",
+      location: "Los Angeles",
+      is_public: true,
+      created_at: "2024-02-01T00:00:00Z",
+      updated_at: "2024-02-01T00:00:00Z",
+      created_by: 2,
+      subscribers: [1, 2, 3],
     },
     {
       id: 3,
-      name: "Jane Fisher",
-      role: "Senior Developer",
-      team: "Development",
+      name: "Event 3",
+      description: "This is the third event",
       status: "active",
-      age: "22",
-      avatar: "https://i.pravatar.cc/150?u=a04258114e29026702d",
-      email: "jane.fisher@example.com",
+      start_date: "2024-03-01T00:00:00Z",
+      end_date: "2024-03-02T00:00:00Z",
+      location: "Chicago",
+      is_public: true,
+      created_at: "2024-03-01T00:00:00Z",
+      updated_at: "2024-03-01T00:00:00Z",
+      created_by: 3,
+      subscribers: [1, 2, 3],
     },
     {
       id: 4,
-      name: "William Howard",
-      role: "Community Manager",
-      team: "Marketing",
-      status: "vacation",
-      age: "28",
-      avatar: "https://i.pravatar.cc/150?u=a048581f4e29026701d",
-      email: "william.howard@example.com",
+      name: "Event 4",
+      description: "This is the fourth event",
+      status: "active",
+      start_date: "2024-04-01T00:00:00Z",
+      end_date: "2024-04-02T00:00:00Z",
+      location: "Houston",
+      is_public: true,
+      created_at: "2024-04-01T00:00:00Z",
+      updated_at: "2024-04-01T00:00:00Z",
+      created_by: 1,
+      subscribers: [1, 2, 3],
     },
     {
       id: 5,
-      name: "Kristen Copper",
-      role: "Sales Manager",
-      team: "Sales",
+      name: "Event 5",
+      description: "This is the fifth event",
       status: "active",
-      age: "24",
-      avatar: "https://i.pravatar.cc/150?u=a092581d4ef9026700d",
-      email: "kristen.cooper@example.com",
+      start_date: "2024-05-01T00:00:00Z",
+      end_date: "2024-05-02T00:00:00Z",
+      location: "Philadelphia",
+      is_public: true,
+      created_at: "2024-05-01T00:00:00Z",
+      updated_at: "2024-05-01T00:00:00Z",
+      created_by: 1,
+      subscribers: [1, 2, 3],
     },
     {
       id: 6,
-      name: "John Smith",
-      role: "Account Manager",
-      team: "Accounting",
+      name: "Event 6",
+      description: "This is the sixth event",
       status: "active",
-      age: "27",
-      avatar: "https://i.pravatar.cc/150?u=a092581d4ef9026700e",
-      email: "john.smith@example.com",
+      start_date: "2024-06-01T00:00:00Z",
+      end_date: "2024-06-02T00:00:00Z",
+      location: "San Francisco",
+      is_public: true,
+      created_at: "2024-06-01T00:00:00Z",
+      updated_at: "2024-06-01T00:00:00Z",
+      created_by: 1,
+      subscribers: [1, 2, 3],
     },
     {
       id: 7,
-      name: "Jane Doe",
-      role: "Marketing Manager",
-      team: "Marketing",
+      name: "Event 7",
+      description: "This is the seventh event",
       status: "active",
-      age: "30",
-      avatar: "https://i.pravatar.cc/150?u=a092581d4ef9026700f",
-      email: "jane.doe@example.com",
+      start_date: "2024-07-01T00:00:00Z",
+      end_date: "2024-07-02T00:00:00Z",
+      location: "Boston",
+      is_public: true,
+      created_at: "2024-07-01T00:00:00Z",
+      updated_at: "2024-07-01T00:00:00Z",
+      created_by: 1,
+      subscribers: [1, 2, 3],
     },
     {
       id: 8,
-      name: "James Johnson",
-      role: "Product Manager",
-      team: "Product",
+      name: "My Event",
+      description: "This is my event",
       status: "active",
-      age: "32",
-      avatar: "https://i.pravatar.cc/150?u=a092581d4ef90267010",
-      email: "james.johnson@example.com",
+      start_date: "2020-01-01T00:00:00Z",
+      end_date: "2020-01-02T00:00:00Z",
+      location: "My Location",
+      is_public: true,
+      created_at: "2023-05-10T17:44:49.337693Z",
+      updated_at: "2023-05-10T17:44:49.337708Z",
+      created_by: 2,
+      subscribers: [1, 2, 3],
     },
     {
       id: 9,
-      name: "Emily Williams",
-      role: "Software Engineer",
-      team: "Engineering",
+      name: "My Event",
+      description: "This is my event",
       status: "active",
-      age: "25",
-      avatar: "https://i.pravatar.cc/150?u=a092581d4ef90267011",
-      email: "emily.williams@example.com",
+      start_date: "2020-01-01T00:00:00Z",
+      end_date: "2020-01-02T00:00:00Z",
+      location: "Milan",
+      is_public: true,
+      created_at: "2023-05-10T17:44:56.523062Z",
+      updated_at: "2023-05-10T17:44:56.523078Z",
+      created_by: 2,
+      subscribers: [1, 2, 3],
     },
     {
       id: 10,
-      name: "David Miller",
-      role: "Data Analyst",
-      team: "Analytics",
+      name: "My Event",
+      description: "This is my event",
       status: "active",
-      age: "28",
-      avatar: "https://i.pravatar.cc/150?u=a092581d4ef90267012",
-      email: "david.miller@example.com",
+      start_date: "2020-01-01T00:00:00Z",
+      end_date: "2020-01-02T00:00:00Z",
+      location: "Milan",
+      is_public: true,
+      created_at: "2023-05-10T17:44:58.373328Z",
+      updated_at: "2023-05-10T17:44:58.373342Z",
+      created_by: 2,
+      subscribers: [1, 2, 3],
     },
     {
       id: 11,
-      name: "Olivia Brown",
-      role: "Graphic Designer",
-      team: "Design",
+      name: "My Event",
+      description: "This is my event",
       status: "active",
-      age: "22",
-      avatar: "https://i.pravatar.cc/150?u=a092581d4ef90267013",
-      email: "olivia.brown@example.com",
+      start_date: "2020-01-01T00:00:00Z",
+      end_date: "2020-01-02T00:00:00Z",
+      location: "Milan",
+      is_public: true,
+      created_at: "2023-05-10T17:45:04.119360Z",
+      updated_at: "2023-05-10T17:45:04.119373Z",
+      created_by: 2,
+      subscribers: [1, 2, 3],
     },
     {
       id: 12,
-      name: "Charles Davis",
-      role: "Project Manager",
-      team: "Projects",
+      name: "Rust Meetup",
+      description: "This is my event",
       status: "active",
-      age: "35",
-      avatar: "https://i.pravatar.cc/150?u=a092581d4ef90267014",
-      email: "charles.davis@example.com",
+      start_date: "2020-01-01T00:00:00Z",
+      end_date: "2020-01-02T00:00:00Z",
+      location: "Milan",
+      is_public: true,
+      created_at: "2023-05-10T17:45:22.795223Z",
+      updated_at: "2023-05-10T17:45:22.795238Z",
+      created_by: 2,
+      subscribers: [1, 2, 3],
     },
     {
       id: 13,
-      name: "Samantha Moore",
-      role: "Content Writer",
-      team: "Content",
+      name: "Rust Meetup",
+      description: "This is my event",
       status: "active",
-      age: "26",
-      avatar: "https://i.pravatar.cc/150?u=a092581d4ef90267015",
-      email: "samantha.moore@example.com",
+      start_date: "2020-01-01T00:00:00Z",
+      end_date: "2020-01-02T00:00:00Z",
+      location: "Milan",
+      is_public: true,
+      created_at: "2023-05-10T17:46:28.385105Z",
+      updated_at: "2023-05-10T17:46:28.385118Z",
+      created_by: 2,
+      subscribers: [1, 2, 3],
     },
     {
       id: 14,
-      name: "Joseph Taylor",
-      role: "Customer Service Rep",
-      team: "Customer Service",
+      name: "Rust Meetup2",
+      description: "This is my event",
       status: "active",
-      age: "31",
-      avatar: "https://i.pravatar.cc/150?u=a092581d4ef90267016",
-      email: "joseph.taylor@example.com",
+      start_date: "2020-01-01T00:00:00Z",
+      end_date: "2020-01-02T00:00:00Z",
+      location: "Milan",
+      is_public: true,
+      created_at: "2023-05-10T17:46:30.222192Z",
+      updated_at: "2023-05-10T17:54:13.651331Z",
+      created_by: 2,
+      subscribers: [1, 2, 3],
     },
     {
-      id: 15,
-      name: "Emily Anderson",
-      role: "Software Developer",
-      team: "Development",
+      id: 16,
+      name: "Rust Meetup",
+      description: "This is my event",
       status: "active",
-      age: "29",
-      avatar: "https://i.pravatar.cc/150?u=a092581d4ef90267017",
-      email: "emily.anderson@example.com",
+      start_date: "2020-01-01T00:00:00Z",
+      end_date: "2020-01-02T00:00:00Z",
+      location: "Milan",
+      is_public: true,
+      created_at: "2023-05-10T20:19:45.785097Z",
+      updated_at: "2023-05-10T20:19:45.785110Z",
+      created_by: 2,
+      subscribers: [1, 3],
+    },
+    {
+      id: 17,
+      name: "Rust Meetup not aurora",
+      description: "This is my event",
+      status: "active",
+      start_date: "2020-01-01T00:00:00Z",
+      end_date: "2020-01-02T00:00:00Z",
+      location: "Milan",
+      is_public: true,
+      created_at: "2023-05-10T20:19:51.311458Z",
+      updated_at: "2023-05-10T20:19:51.311471Z",
+      created_by: 2,
+      subscribers: [1, 2, 3],
+    },
+    {
+      id: 18,
+      name: "Rust Meetup not aurora",
+      description: "This is my event",
+      status: "active",
+      start_date: "2020-01-01T00:00:00Z",
+      end_date: "2020-01-02T00:00:00Z",
+      location: "Milan",
+      is_public: true,
+      created_at: "2023-05-10T20:41:10.988080Z",
+      updated_at: "2023-05-10T20:41:10.988093Z",
+      created_by: 2,
+      subscribers: [1, 3],
+    },
+    {
+      id: 19,
+      name: "Rust Meetup not aurora",
+      description: "This is my event",
+      status: "active",
+      start_date: "2024-01-01T00:00:00Z",
+      end_date: "2024-01-02T00:00:00Z",
+      location: "Milan",
+      is_public: true,
+      created_at: "2023-05-10T20:41:28.376963Z",
+      updated_at: "2023-05-10T20:41:28.376976Z",
+      created_by: 2,
+      subscribers: [1, 3],
     },
   ];
-  const renderCell = (user, columnKey) => {
-    const cellValue = user[columnKey];
+
+  const renderCell = (event, columnKey) => {
+    const cellValue = event[columnKey];
     switch (columnKey) {
       case "name":
         return (
-          <User
-            squared
-            src={user.avatar}
-            name={cellValue}
-            css={{ p: 0 }}>
-            {user.email}
-          </User>
+          <Row justify="center" align="center">
+             <Text b size={14} css={{ tt: "capitalize" }}> 
+              {cellValue} 
+            </Text>
+          </Row>
         );
       case "role":
         return (
@@ -200,15 +309,31 @@ export const Events = () => {
                 b
                 size={13}
                 css={{ tt: "capitalize", color: "$accents7" }}>
-                {user.team}
+                {event.team}
               </Text>
             </Row>
           </Col>
         );
       case "status":
         return (
-          <StyledBadge type={user.status}>{cellValue}</StyledBadge>
+          <StyledBadge type={event.status}>{cellValue}</StyledBadge>
         );
+
+      case "start_date":
+        return (
+          <Text>
+          {dayjs(cellValue).format("DD/MM/YYYY")}
+          </Text>
+        );
+
+      case "end_date":
+
+        return (
+          <Text>
+          {dayjs(cellValue).format("DD/MM/YYYY")}
+          </Text>
+        );
+
 
       case "actions":
         return (
@@ -217,17 +342,17 @@ export const Events = () => {
               <Tooltip content="Details">
                 <IconButton
                   onClick={() =>
-                    console.log("View user", user.id)
+                    console.log("View event", event.id)
                   }>
                   <EyeIcon size={20} fill="#979797" />
                 </IconButton>
               </Tooltip>
             </Col>
             <Col css={{ d: "flex" }}>
-              <Tooltip content="Edit user">
+              <Tooltip content="Edit event">
                 <IconButton
                   onClick={() =>
-                    console.log("Edit user", user.id)
+                    console.log("Edit event", event.id)
                   }>
                   <EditIcon size={20} fill="#979797" />
                 </IconButton>
@@ -235,10 +360,10 @@ export const Events = () => {
             </Col>
             <Col css={{ d: "flex" }}>
               <Tooltip
-                content="Delete user"
+                content="Delete event"
                 color="error"
                 onClick={() =>
-                  console.log("Delete user", user.id)
+                  console.log("Delete event", event.id)
                 }>
                 <IconButton>
                   <DeleteIcon size={20} fill="#FF0080" />
@@ -264,12 +389,12 @@ export const Events = () => {
           <Table.Column
             key={column.uid}
             hideHeader={column.uid === "actions"}
-            align={column.uid === "actions" ? "center" : "start"}>
+            align={_.includes(["actions", "name"], column.uid) ? "center" : "start"}>
             {column.name}
           </Table.Column>
         )}
       </Table.Header>
-      <Table.Body items={users}>
+      <Table.Body items={events}>
         {item => (
           <Table.Row>
             {columnKey => (
